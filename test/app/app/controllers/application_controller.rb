b3b59cda370c32229f64d046078726218f7b46be
Jon Leighton <j@jonathanleighton.com>
2012-02-18 17:33:04 +0000

    include FocusedController::Mixin rather than FocusedController to avoid potential constant resolution issues for users

--- test/app/app/controllers/application_controller.rb
+++ test/app/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  include FocusedController
+  include FocusedController::Mixin
 end
---
+++ test/unit/mixin_test.rb
@@ -0,0 +1,53 @@
+require 'helper'
+
+module FocusedController
+  module Test
+    class MixinTestController
+      include FocusedController::Mixin
+
+      class << self
+        attr_accessor :name
+      end
+    end
+  end
+end
+
+module FocusedController
+  describe Mixin do
+    describe "with a PostsController::Show class" do
+      subject do
+        klass = Class.new(FocusedController::Test::MixinTestController)
+        klass.name = "PostsController::Show"
+        klass.new
+      end
+
+      it "has a .controller_name of 'posts'" do
+        subject.class.controller_path.must_equal 'posts'
+      end
+
+      it "has an #action_name of 'show'" do
+        subject.action_name.must_equal 'show'
+      end
+
+      it "uses the run method for the action" do
+        subject.method_for_action('whatever').must_equal 'run'
+      end
+    end
+
+    describe "with a Posts::ShowController class" do
+      subject do
+        klass = Class.new(FocusedController::Test::MixinTestController)
+        klass.name = "Posts::ShowController"
+        klass.new
+      end
+
+      it "has a .controller_name of 'posts'" do
+        subject.class.controller_path.must_equal 'posts'
+      end
+
+      it "has an #action_name of 'show'" do
+        subject.action_name.must_equal 'show'
+      end
+    end
+  end
+end
