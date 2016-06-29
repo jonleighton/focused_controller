99274a327cd2f1a23bfb4fd3de5eb7dc49cb22e4
Jon Leighton <j@jonathanleighton.com>
2016-06-29 13:57:11 +0100

    RSpec 2 compatibility
    The app used for the end-to-end tests uses RSpec 2, even though the
    unit tests will use the latest version of RSpec. In general it seems
    good not to break backwards compatibility, and so to support both
    versions.

--- lib/focused_controller/rspec_controller_class.rb
+++ lib/focused_controller/rspec_controller_class.rb
@@ -1,12 +1,20 @@
+require "rspec/version"
+
 module FocusedController
   module RSpecControllerClass
     def controller_class
       metadata = self.metadata
+      metadata = metadata[:example_group] if RSpec::Version::STRING < "3"
       klass    = nil
 
       until metadata.nil? || klass.respond_to?(:new)
         klass    = metadata[:description_args].first
-        metadata = metadata[:parent_example_group]
+
+        if RSpec::Version::STRING < "3"
+          metadata = metadata[:example_group]
+        else
+          metadata = metadata[:parent_example_group]
+        end
       end
 
       klass.respond_to?(:new) ? klass : super
